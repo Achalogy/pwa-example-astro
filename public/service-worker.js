a921628e0f58cd71198528bbde7d0a177b3a36fc
Achalogy <achalogy@gmail.com>
2024-10-31 20:21:56 -0500

    update worker

---
+++ public/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Archivos en caché correctamente');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Limpiando caché antigua');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
