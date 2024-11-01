66f7143689b98e787fa40d6372bec0e4536a90ab
Achalogy <achalogy@gmail.com>
2024-10-31 20:26:35 -0500

    try  to fix it

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,11 +27,11 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
-});
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then((response) => {
+//         return response || fetch(event.request);
+//       })
+//   );
+// });
